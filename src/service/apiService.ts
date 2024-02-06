7d125557326297726d95cf15f83ba03b004563af
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-02-06 15:42:18 +0100

    tsconfig

--- src/service/apiService.ts
+++ src/service/apiService.ts
@@ -1,18 +1,8 @@
 import axios from 'axios';
-// import { Dispatch } from 'redux';
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import { RentalCars } from '../redux/types';
 
 const instance = axios.create({
   baseURL: 'https://65baa122b4d53c0665535a23.mockapi.io',
 });
 
-// const getCars = createAsyncThunk<RentalCars[], number>(
-//   'cars/getAllCars',
-//   async (page: number) => {
-//     const response = await instance.get(`/adverts/?limit=12&page=${page}`);
-//     return response.data;
-//   }
-// );, getCars 
 
 export { instance};
