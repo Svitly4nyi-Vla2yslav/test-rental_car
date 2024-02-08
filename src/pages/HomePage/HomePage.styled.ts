d619d2856e1c12287bde5f1da9f4eb70036d351a
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-02-08 14:26:15 +0100

    background image

--- src/pages/HomePage/HomePage.styled.ts
+++ src/pages/HomePage/HomePage.styled.ts
@@ -1,14 +1,12 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.main`
-background-image: url('/src/assets/wp11965249.jpg'); 
-background-size: cover;
-background-position: center;
-background-size:  100%;
-background-position:  center;
-background-repeat: no-repeat;
-width: 100%;
-height: 925px;
-padding-top: 50px;
-background-attachment: fixed;
-`
+  background-image: url('https://wallpapercave.com/wp/wp11965249.jpg'); 
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 925px;
+  padding-top: 50px;
+  background-attachment: fixed;
+`;
